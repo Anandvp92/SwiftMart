a393c2b5007f18b7062d736b449a33c943e32699
Anandvp <anandvp92@gmail.com>
2024-03-18 15:53:22 +0530

    added the loginpage twiks

--- SwiftMart/usermanagement/urls.py
+++ SwiftMart/usermanagement/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register,loginuser,logoutuser
+from .views import register,loginuser,logoutuser,restpassword
 urlpatterns=[
     path('register/',register,name='register'),
     path('login/',loginuser,name='login'),
     path('logout/',logoutuser,name='logoutuser'),
+    path('resetpass/',restpassword,name='resetpass'),
 ]
